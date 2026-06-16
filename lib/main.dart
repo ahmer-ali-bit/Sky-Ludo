3281a71becb7bb90bc7769d8262fc6b40d09c943
tk-devv <mtalhaa861@gmail.com>
2026-06-17 02:59:00 +0500

    add coin_offer_file

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'coin_offer.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -13,7 +15,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter luddo app',
       theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const CoinOffer(),
     );
   }
 }
